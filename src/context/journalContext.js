d1161f67e7a255a794510d88d5c071ac3af26740
ivyreee <ivy@drive.digital>
2024-06-12 21:24:13 +0800

    time sorting

--- src/context/journalContext.js
+++ src/context/journalContext.js
@@ -11,8 +11,7 @@ const data = [
       "The weather was perfect, slightly cool with a gentle breeze. It felt invigorating to breathe in the fresh air and clear my mind. I pushed myself to go a little further than usual, and by the time I got home, I felt both exhausted and accomplished.",
       "It’s amazing how much a good run can improve my mood."
     ],
-    'date': 'June 12',
-    'time': '10:32AM'
+    'date': '2024-12-12T01:59:00.000Z'
   },
   {
     'title': '',
@@ -22,8 +21,7 @@ const data = [
       "We hiked up to a beautiful waterfall where we spent a couple of hours just soaking in the natural beauty. At night, the stars were so bright and numerous that it felt like you could reach out and touch them.",
       "It was a refreshing break from the daily grind and a reminder of how important it is to connect with nature every now and then."
     ],
-    'date': 'June 11',
-    'time': '3:15PM'
+    'date': '2024-12-12T02:59:00.000Z'
   },
   {
     'title': 'Art Therapy',
@@ -33,8 +31,7 @@ const data = [
       "It's challenging but incredibly rewarding to see the colors blend and the scene come to life on the canvas. I lost track of time and spent hours adjusting the hues and shadows to get it just right.",
       "Art has always been my escape, and today was a perfect example of how therapeutic it can be."
     ],
-    'date': 'June 11',
-    'time': '1:47PM'
+    'date': '2024-06-13T15:59:00.000Z'
   },
   {
     'title': '',
@@ -44,8 +41,7 @@ const data = [
       "I had to juggle multiple tasks and coordinate with different departments to ensure everything came together smoothly. By the end of the day, we managed to submit the project just in time.",
       "It was stressful, but the sense of accomplishment once we were done was worth it. Now I can finally relax."
     ],
-    'date': 'June 10',
-    'time': '6:22PM'
+    'date': '2024-06-13T15:59:00.000Z'
   },
   {
     'title': 'Castle Dream',
@@ -55,18 +51,29 @@ const data = [
       "Each room had its own unique theme, from a library with endless books to a ballroom frozen in time. I felt a sense of adventure and curiosity, like I was on a quest.",
       "It was one of those dreams that felt incredibly real, and I woke up feeling inspired to add more creativity and exploration to my everyday life."
     ],
-    'date': 'June 10',
-    'time': '7:05AM'
+    'date': '2024-12-25T02:59:00.000Z'
   }
 ];
+
+const sorted = data.sort(function(a, b){
+  return -a.date.localeCompare(b.date)
+})
 
 export const useJournal = () => useContext(JournalContext);
 
 export const JournalProvider = ({ children }) => {
-  const [journalEntries, setJournalEntries] = useState(data);
+  const [journalEntries, setJournalEntries] = useState(sorted);
 
   const addJournalEntry = (newEntry) => {
-    setJournalEntries((prevEntries) => [...prevEntries, newEntry]);
+    setJournalEntries((prevEntries) => {
+
+      const newData = [...prevEntries, newEntry];
+
+      return newData.sort(function(a, b){
+        return -a.date.localeCompare(b.date)
+      })
+
+    });
   };
 
   return (
--- src/pages/calendar.jsx
+++ src/pages/calendar.jsx
@@ -2,14 +2,16 @@ import Calendar from "react-calendar"
 import Entry from "../components/entry"
 import { useTheme } from "../context/themeContext";
 import { useJournal } from "../context/journalContext";
+import Moment from "react-moment";
+import moment from "moment";
 
 const CalendarPage = () => {
 
     const { outline } = useTheme(),
-        { journalEntries } = useJournal();
+        { journalEntries, addJournalEntry } = useJournal();
 
     const group = journalEntries.reduce((group, entry) => {
-        const { date } = entry;
+        const date = moment(entry.date).format('MMM DD');
         if (!group[date]) {
             group[date] = [];
         }
@@ -17,13 +19,27 @@ const CalendarPage = () => {
         return group;
     }, {});
 
+    const newEntry = {
+        'title': 'Test',
+        'subtitle': 'Omoide Trigger 5th Anniversary',
+        'notes': [
+            "Last night, I had the most vivid dream. I was exploring a huge, ancient castle filled with hidden passageways and secret rooms."
+        ],
+        'date': '2024-12-25T11:00:00.000Z'
+    }
+
     return (
         <>
             <Calendar />
 
-
+            <p onClick={() => addJournalEntry(newEntry)}>new entry</p>
 
             {Object.entries(group).map(item => {
+
+                console.log(moment(item[0]).format('MMM DD'))
+                console.log(moment(item[0]).format('hh:mm A'))
+
+                // const date = moment(item[0]).format('MMM DD')
 
                 return (
                     <>
