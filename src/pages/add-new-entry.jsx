b31526506aa4e16f1f74ffbdd5a863d1fa88c055
ivyreee <ivy@drive.digital>
2024-06-14 02:40:46 +0800

    editor.js

--- src/pages/add-new-entry.jsx
+++ src/pages/add-new-entry.jsx
@@ -3,13 +3,18 @@ import { useTheme } from "../context/themeContext"
 import { useJournal } from "../context/journalContext";
 import { useNavigate } from "react-router";
 
+import EditorJS from '@editorjs/editorjs';
+import { useEffect } from "react";
 
+import Header from '@editorjs/header';
+import Quote from '@editorjs/quote';
+import SimpleImage from "@editorjs/simple-image";
 
 const NewEntry = () => {
 
     const { outline } = useTheme(),
-    { addJournalEntry} = useJournal(),
-    navigate = useNavigate();
+        { addJournalEntry } = useJournal(),
+        navigate = useNavigate();
 
     const addNewEntry = (e) => {
         e.preventDefault()
@@ -25,25 +30,26 @@ const NewEntry = () => {
         navigate('/');
     }
 
+    useEffect(() => {
+        const editor = new EditorJS({
+            holder: 'Editor',
+            tools: {
+                header: { class: Header },
+                image: { class: SimpleImage }
+            }
+        });
+    })
+
     return (
         <>
             <h1>How's your day?</h1>
             <h2>{moment().format('MMM DD')}</h2>
-            <form onSubmit={(e) => addNewEntry(e)}>
-                <div class='form-input'>
-                    <input class={`${outline ? 'outline' : 'no-outline'}`} type='text' name='title' placeholder="Title"></input>
-                </div>
-                <div class='form-input'>
-                    <input class={`${outline ? 'outline' : 'no-outline'}`} type='text' name='subtitle' placeholder="Topic"></input>
-                </div>
-                <div class='form-input'>
-                    <textarea class={`${outline ? 'outline' : 'no-outline'}`} name='notes' placeholder="Write your entry here."></textarea>
-                </div>
-                <button type='submit' class={`${outline ? 'outline' : 'no-outline'}`}>Submit</button>
-            </form>
+            <hr />
+            <div id='Editor'></div>
         </>
     )
 
 }
 
 export default NewEntry
+
