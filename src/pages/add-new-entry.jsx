ee8b85be18a6b0299f6f02ca9499096e2f0b7aa6
ivyreee <ivy@drive.digital>
2024-06-14 03:24:44 +0800

    .

--- src/pages/add-new-entry.jsx
+++ src/pages/add-new-entry.jsx
@@ -38,12 +38,27 @@ const NewEntry = () => {
                 image: { class: SimpleImage }
             }
         });
-    })
+
+        function SaveEntry() {
+            editor.save().then((output) => {
+
+                console.log(output)
+
+            }).catch((error) => {
+                console.log('Saving failed: ', error)
+            });
+        }
+
+        document.querySelector('#submit').addEventListener('click', SaveEntry)
+
+    }, [])
 
     return (
         <>
-            <h1>How's your day?</h1>
-            <h2>{moment().format('MMM DD')}</h2>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                <h1>{moment().format('MMM DD')}</h1>
+                <button id='submit' style={{ flex: '0 1 30%' }}>Save</button>
+            </div>
             <hr />
             <div id='Editor'></div>
         </>
