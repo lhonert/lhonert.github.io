ba46ab650f53379daf0f33c870bb8079e3986859
ivyreee <ivy@drive.digital>
2024-06-14 20:29:57 +0800

    styling fixes

--- src/context/themeContext.js
+++ src/context/themeContext.js
@@ -6,7 +6,7 @@ export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('beby');
-  const [outline, setOutline] = useState(true);
+  const [outline, setOutline] = useState(false);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme, outline, setOutline }}>
